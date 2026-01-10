83dbd1fc6b38cebed6ca568e3b9767d5fd13ff04
Rishindu Yohan <rishinduyohan@gmail.com>
2026-01-10 16:56:47 +0530

    create delete product function in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 // console.log("Test js..");
 const productGrid = document.getElementById('productGrid');
+const API_URL = 'https://dummyjson.com/products';
 let products = [];
 
-fetch('https://dummyjson.com/products')
+fetch(API_URL+'?limit=16')
     .then(res => res.json())
     .then(data => {
         products = data;
@@ -10,7 +11,7 @@ fetch('https://dummyjson.com/products')
         data.products.forEach(product => {
             const productCard = document.createElement('div');
             productCard.className = 'product-card';
-            productCard.classList = 'card';
+            productCard.classList.add('card');
             productCard.innerHTML = `
                 <img src="${product.thumbnail}" class="card-img-top" alt="${product.title}">
                 <div class="card-body">
@@ -22,12 +23,35 @@ fetch('https://dummyjson.com/products')
                     <li class="list-group-item">$${product.price}</li>
                     <li class="list-group-item">${product.availabilityStatus}</li>
                 </ul>
-                <div class="card-body">
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
+                <div class="card-footer bg-white border-top-0 d-flex gap-2">
+                <button class="btn btn-outline-primary flex-grow-1" onclick="editProduct(${product.id})">
+                    <i class="fa-solid fa-pen-to-square"></i> Edit
+                </button>
+                <button class="btn btn-outline-danger" onclick="deleteProduct(${product.id}, this)">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </div>
             `;
             productGrid.appendChild(productCard);
         });
     });
 
+editProduct = (id) => {
+    console.log(id);
+
+}
+
+async function deleteProduct(id, btn) {
+    if (confirm("Are you sure you want to delete this product?")) {
+        try {
+            const response = await fetch(API_URL+`/${id}`, {method: 'DELETE'});
+
+            if(response.status===200){
+                btn.closest('.product-card').remove();
+                alert("Successfully deleted!");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
