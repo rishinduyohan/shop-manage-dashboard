5ab2131daff9f683f4a6c4e841c3a837c295e510
Rishindu Yohan <rishinduyohan@gmail.com>
2026-01-10 15:34:42 +0530

    create hero section with add product button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,2 @@
 console.log("Test js..");
 
-const modal = document.getElementById('productModal');
-        const openBtn = document.getElementById('openAddModal');
-        const closeBtn = document.getElementById('closeModal');
-        const cancelBtn = document.getElementById('cancelBtn');
-
-        const toggleModal = () => modal.classList.toggle('hidden');
-
-        openBtn.addEventListener('click', () => {
-            document.getElementById('productForm').reset();
-            document.getElementById('modalTitle').innerText = 'Add New Product';
-            toggleModal();
-        });
-
-        [closeBtn, cancelBtn].forEach(btn => btn.addEventListener('click', toggleModal));
